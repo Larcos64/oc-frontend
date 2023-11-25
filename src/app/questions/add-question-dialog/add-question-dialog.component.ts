d76602d9c09dee2dc624a9b86a53aec329dbcc9c
@andresFelipeGuevara <andresguevara1601@gmail.com>
2023-11-25 08:57:17 -0500

    correccion de algunos errores

--- src/app/questions/add-question-dialog/add-question-dialog.component.ts
+++ src/app/questions/add-question-dialog/add-question-dialog.component.ts
@@ -1,22 +1,24 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatSnackBar } from '@angular/material';
-import { MatSlideToggleChange } from '@angular/material/slide-toggle';
-import { MAT_DIALOG_DATA, MatDialogRef } from '../../../../node_modules/@angular/material';
-import { finalize } from 'rxjs/operators';
-import { ActQues } from '../../shared/models/actquestion.model';
-import { snackError, snackOk } from '../../util/snackbar-util';
-import { QuestionsService } from '../services/questions.service';
-import { OptionsService } from '../../options/services/options.service';
-import { QuestypesService, Type } from '../services/questypes.service';
+import { Component, OnInit, Inject } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MatSnackBar } from "@angular/material";
+import { MatSlideToggleChange } from "@angular/material/slide-toggle";
+import {
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+} from "../../../../node_modules/@angular/material";
+import { finalize } from "rxjs/operators";
+import { ActQues } from "../../shared/models/actquestion.model";
+import { snackError, snackOk } from "../../util/snackbar-util";
+import { QuestionsService } from "../services/questions.service";
+import { OptionsService } from "../../options/services/options.service";
+import { QuestypesService, Type } from "../services/questypes.service";
 
 @Component({
-  selector: 'app-add-question-dialog',
-  templateUrl: './add-question-dialog.component.html',
-  styleUrls: ['./add-question-dialog.component.scss']
+  selector: "app-add-question-dialog",
+  templateUrl: "./add-question-dialog.component.html",
+  styleUrls: ["./add-question-dialog.component.scss"],
 })
 export class AddQuestionDialogComponent implements OnInit {
-
   form: FormGroup;
   question = new ActQues();
   options = new Array();
@@ -28,9 +30,15 @@ export class AddQuestionDialogComponent implements OnInit {
   listTypes: Type[] = [];
   styles: number[] = [1, 2, 3];
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private fb: FormBuilder, public service: QuestionsService,
-    public serviceOpt: OptionsService, public serviceQt: QuestypesService, private snackbar: MatSnackBar,
-    public dialogRef: MatDialogRef<AddQuestionDialogComponent>) { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private fb: FormBuilder,
+    public service: QuestionsService,
+    public serviceOpt: OptionsService,
+    public serviceQt: QuestypesService,
+    private snackbar: MatSnackBar,
+    public dialogRef: MatDialogRef<AddQuestionDialogComponent>
+  ) {}
 
   ngOnInit() {
     this.initForm();
@@ -41,71 +49,94 @@ export class AddQuestionDialogComponent implements OnInit {
   initForm() {
     this.form = this.fb.group({
       idSec: [parseInt(this.data.idSec), Validators.required],
-      idOpt: [''],
-      nameQues: ['', [Validators.required, Validators.maxLength(300)]],
-      descQues: ['', Validators.maxLength(500)],
-      typeQues: ['', Validators.required],
-      infoQues: [''],
+      idOpt: [""],
+      nameQues: ["", [Validators.required, Validators.maxLength(300)]],
+      descQues: ["", Validators.maxLength(500)],
+      typeQues: ["", Validators.required],
+      infoQues: [""],
       mandatoryQues: [this.mandQuest, Validators.required],
-      itemValue: [''],
-      orderQues: [this.data.orderQues, Validators.required]
-
+      itemValue: [""],
+      orderQues: [this.data.orderQues, Validators.required],
     });
   }
 
   // convenience getter for easy access to form fields
-  get f() { return this.form.controls; }
+  get f() {
+    return this.form.controls;
+  }
 
   getQuesTypes() {
     this.listTypes = this.serviceQt.getTypes();
+    console.log(this.listTypes);
+    this.listTypes = [
+      {
+        value: "radio_button_checked",
+        type: "Varias opciones",
+        icon: "radio_button_checked",
+      },
+      { value: "check_box", type: "Casillas", icon: "check_box" },
+      {
+        value: "arrow_drop_down_circle",
+        type: "Desplegable",
+        icon: "arrow_drop_down_circle",
+      },
+    ];
   }
 
   getOptions() {
-    this.serviceOpt.list().subscribe(options => {
+    this.serviceOpt.list().subscribe((options) => {
       this.options = options;
+      console.log(this.options);
     });
   }
 
   changeMandatory(ob: MatSlideToggleChange) {
     if (ob.checked) {
       this.mandQuest = true;
-      this.form.get('mandatoryQues').setValue(this.mandQuest);
+      this.form.get("mandatoryQues").setValue(this.mandQuest);
     } else {
       this.mandQuest = false;
-      this.form.get('mandatoryQues').setValue(this.mandQuest);
+      this.form.get("mandatoryQues").setValue(this.mandQuest);
     }
   }
 
   onTypeChange(tp) {
     this.selectedType = tp.value;
-    if (this.selectedType === "radio_button_checked" || this.selectedType === "check_box" || this.selectedType === "arrow_drop_down_circle") {
+    if (
+      this.selectedType === "radio_button_checked" ||
+      this.selectedType === "check_box" ||
+      this.selectedType === "arrow_drop_down_circle"
+    ) {
       this.avOptions = true;
-      this.form.get('idOpt').setValidators([Validators.required]);
-      this.form.get('idOpt').enable();
-      this.form.get('idOpt').updateValueAndValidity();
+      this.form.get("idOpt").setValidators([Validators.required]);
+      this.form.get("idOpt").enable();
+      this.form.get("idOpt").updateValueAndValidity();
     } else {
       this.avOptions = false;
-      this.form.get('idOpt').clearValidators();
-      this.form.get('idOpt').disable();
-      this.form.get('idOpt').updateValueAndValidity();
+      this.form.get("idOpt").clearValidators();
+      this.form.get("idOpt").disable();
+      this.form.get("idOpt").updateValueAndValidity();
     }
   }
 
   saveQuestion(dataOption) {
     if (dataOption) {
-      this.form.get('idOpt').setValue(dataOption.idOption);
+      this.form.get("idOpt").setValue(dataOption.idOption);
     }
 
     this.question = new ActQues(this.form.value);
     this.loading = true;
-    (this.service.add(this.question)).pipe(
-      finalize(() => this.loading = false)
-    ).subscribe(() => this.addOk(), (err) => snackError(this.snackbar, err));
+    this.service
+      .add(this.question)
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe(
+        () => this.addOk(),
+        (err) => snackError(this.snackbar, err)
+      );
   }
 
   addOk() {
-    snackOk(this.snackbar, 'Pregunta registrada');
+    snackOk(this.snackbar, "Pregunta registrada");
     this.dialogRef.close();
   }
-
 }
--- src/app/questions/edit-question-dialog/edit-question-dialog.component.ts
+++ src/app/questions/edit-question-dialog/edit-question-dialog.component.ts
@@ -1,23 +1,22 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { finalize } from 'rxjs/operators';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { MatSlideToggleChange } from '@angular/material/slide-toggle';
-import { QuestionsService } from '../services/questions.service';
-import { OptionsService } from '../../options/services/options.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
-import { ActQues } from '../../shared/models/actquestion.model';
-import { snackError, snackOk } from '../../util/snackbar-util';
-import { MatSnackBar } from '@angular/material';
-import { QuestypesService, Type } from '../services/questypes.service';
+import { Component, OnInit, Inject } from "@angular/core";
+import { finalize } from "rxjs/operators";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { MatSlideToggleChange } from "@angular/material/slide-toggle";
+import { QuestionsService } from "../services/questions.service";
+import { OptionsService } from "../../options/services/options.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
+import { ActQues } from "../../shared/models/actquestion.model";
+import { snackError, snackOk } from "../../util/snackbar-util";
+import { MatSnackBar } from "@angular/material";
+import { QuestypesService, Type } from "../services/questypes.service";
 
 @Component({
-  selector: 'app-edit-question-dialog',
-  templateUrl: './edit-question-dialog.component.html',
-  styleUrls: ['./edit-question-dialog.component.scss']
+  selector: "app-edit-question-dialog",
+  templateUrl: "./edit-question-dialog.component.html",
+  styleUrls: ["./edit-question-dialog.component.scss"],
 })
 export class EditQuestionDialogComponent implements OnInit {
-
   form: FormGroup;
   question = new ActQues();
   options = new Array();
@@ -29,9 +28,17 @@ export class EditQuestionDialogComponent implements OnInit {
   listTypes: Type[] = [];
   styles: number[] = [1, 2, 3];
 
-  constructor(private router: Router, private fb: FormBuilder, public dialogRef: MatDialogRef<EditQuestionDialogComponent>,
-    private snackbar: MatSnackBar, private route: ActivatedRoute, @Inject(MAT_DIALOG_DATA) public data: any,
-    public service: QuestionsService, public serviceQt: QuestypesService, public serviceOpt: OptionsService) { }
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<EditQuestionDialogComponent>,
+    private snackbar: MatSnackBar,
+    private route: ActivatedRoute,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public service: QuestionsService,
+    public serviceQt: QuestypesService,
+    public serviceOpt: OptionsService
+  ) {}
 
   ngOnInit() {
     this.initForm();
@@ -45,25 +52,43 @@ export class EditQuestionDialogComponent implements OnInit {
       idQues: [this.data.idQues, Validators.required],
       idSec: [this.data.idSec, Validators.required],
       idOpt: [this.data.idOpt],
-      nameQues: [this.data.nameQues, [Validators.required, Validators.maxLength(300)]],
+      nameQues: [
+        this.data.nameQues,
+        [Validators.required, Validators.maxLength(300)],
+      ],
       descQues: [this.data.descQues, Validators.maxLength(500)],
       typeQues: [this.data.typeQues, Validators.required],
       infoQues: [this.data.infoQues],
       mandatoryQues: [this.mandQuest, Validators.required],
       itemValue: [this.data.itemValue],
-      orderQues:[this.data.orderQues]
+      orderQues: [this.data.orderQues],
     });
   }
 
   // convenience getter for easy access to form fields
-  get f() { return this.form.controls; }
+  get f() {
+    return this.form.controls;
+  }
 
   getQuesTypes() {
     this.listTypes = this.serviceQt.getTypes();
+    this.listTypes = [
+      {
+        value: "radio_button_checked",
+        type: "Varias opciones",
+        icon: "radio_button_checked",
+      },
+      { value: "check_box", type: "Casillas", icon: "check_box" },
+      {
+        value: "arrow_drop_down_circle",
+        type: "Desplegable",
+        icon: "arrow_drop_down_circle",
+      },
+    ];
   }
 
   getOptions() {
-    this.serviceOpt.list().subscribe(options => {
+    this.serviceOpt.list().subscribe((options) => {
       this.options = options;
     });
   }
@@ -71,52 +96,59 @@ export class EditQuestionDialogComponent implements OnInit {
   changeMandatory(ob: MatSlideToggleChange) {
     if (ob.checked) {
       this.mandQuest = true;
-      this.form.get('mandatoryQues').setValue(this.mandQuest);
+      this.form.get("mandatoryQues").setValue(this.mandQuest);
     } else {
       this.mandQuest = false;
-      this.form.get('mandatoryQues').setValue(this.mandQuest);
+      this.form.get("mandatoryQues").setValue(this.mandQuest);
     }
   }
 
   validateOption() {
     if (this.data.idOpt) {
       this.avOptions = true;
-      this.form.get('idOpt').setValidators([Validators.required]);
-      this.form.get('idOpt').enable();
-      this.form.get('idOpt').updateValueAndValidity();
+      this.form.get("idOpt").setValidators([Validators.required]);
+      this.form.get("idOpt").enable();
+      this.form.get("idOpt").updateValueAndValidity();
     }
   }
 
   onTypeChange(tp) {
     this.selectedType = tp.value;
-    if (this.selectedType === "radio_button_checked" || this.selectedType === "check_box" || this.selectedType === "arrow_drop_down_circle") {
+    if (
+      this.selectedType === "radio_button_checked" ||
+      this.selectedType === "check_box" ||
+      this.selectedType === "arrow_drop_down_circle"
+    ) {
       this.avOptions = true;
-      this.form.get('idOpt').setValidators([Validators.required]);
-      this.form.get('idOpt').enable();
-      this.form.get('idOpt').updateValueAndValidity();
+      this.form.get("idOpt").setValidators([Validators.required]);
+      this.form.get("idOpt").enable();
+      this.form.get("idOpt").updateValueAndValidity();
     } else {
       this.avOptions = false;
-      this.form.get('idOpt').clearValidators();
-      this.form.get('idOpt').disable();
-      this.form.get('idOpt').updateValueAndValidity();
+      this.form.get("idOpt").clearValidators();
+      this.form.get("idOpt").disable();
+      this.form.get("idOpt").updateValueAndValidity();
     }
   }
 
   editQuestion(dataOption) {
     if (dataOption) {
-      this.form.get('idOpt').setValue(dataOption.idOption);
+      this.form.get("idOpt").setValue(dataOption.idOption);
     }
 
     this.question = new ActQues(this.form.value);
     this.loading = true;
-    (this.service.edit(this.question)).pipe(
-      finalize(() => this.loading = false)
-    ).subscribe(() => this.editOk(), (err) => snackError(this.snackbar, err));
+    this.service
+      .edit(this.question)
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe(
+        () => this.editOk(),
+        (err) => snackError(this.snackbar, err)
+      );
   }
 
   editOk() {
-    snackOk(this.snackbar, 'Pregunta actualizada');
+    snackOk(this.snackbar, "Pregunta actualizada");
     this.dialogRef.close();
   }
-
 }
