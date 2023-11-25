80396e3c84e83233edf0ef2e3389f900bd268ded
@andresFelipeGuevara <andresguevara1601@gmail.com>
2023-11-25 00:21:37 -0500

    seccion cambio en la lista y pruebas

--- src/app/sections/edit-section-dialog/edit-section-dialog.component.ts
+++ src/app/sections/edit-section-dialog/edit-section-dialog.component.ts
@@ -1,27 +1,33 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { finalize } from 'rxjs/operators';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { SectionsService } from '../services/sections.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
-import { ActSection } from '../../shared/models/actsection.model';
-import { snackError, snackOk } from '../../util/snackbar-util';
-import { MatSnackBar } from '@angular/material';
-import { sectionsRoutes } from '../sections.routing';
+import { Component, OnInit, Inject } from "@angular/core";
+import { finalize } from "rxjs/operators";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { SectionsService } from "../services/sections.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
+import { ActSection } from "../../shared/models/actsection.model";
+import { snackError, snackOk } from "../../util/snackbar-util";
+import { MatSnackBar } from "@angular/material";
+import { sectionsRoutes } from "../sections.routing";
 
 @Component({
-  selector: 'app-edit-section-dialog',
-  templateUrl: './edit-section-dialog.component.html',
-  styleUrls: ['./edit-section-dialog.component.scss']
+  selector: "app-edit-section-dialog",
+  templateUrl: "./edit-section-dialog.component.html",
+  styleUrls: ["./edit-section-dialog.component.scss"],
 })
 export class EditSectionDialogComponent implements OnInit {
-
   form: FormGroup;
   section = new ActSection();
   loading = false;
 
-  constructor(private router: Router, private fb: FormBuilder, public dialogRef: MatDialogRef<EditSectionDialogComponent>,
-    private snackbar: MatSnackBar, private route: ActivatedRoute, @Inject(MAT_DIALOG_DATA) public data: any, public service: SectionsService) { }
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<EditSectionDialogComponent>,
+    private snackbar: MatSnackBar,
+    private route: ActivatedRoute,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public service: SectionsService
+  ) {}
 
   ngOnInit() {
     this.initForm();
@@ -29,30 +35,40 @@ export class EditSectionDialogComponent implements OnInit {
 
   initForm() {
     this.form = this.fb.group({
-      idSection:   [this.data.idSection, Validators.required],
-      nameSection: [this.data.nameSection, [Validators.required, Validators.maxLength(200)]],
+      idSection: [this.data.idSection, Validators.required],
+      nameSection: [
+        this.data.nameSection,
+        [Validators.required, Validators.maxLength(200)],
+      ],
       descSection: [this.data.descSection, Validators.maxLength(200)],
       typeSection: [this.data.typeSection, Validators.maxLength(10)],
-      tableName:   [this.data.tableName, [Validators.required, Validators.maxLength(20)]],
-      cycle:       [this.data.cycle, [Validators.required]]
+      tableName: [
+        this.data.tableName,
+        [Validators.required, Validators.maxLength(20)],
+      ],
     });
   }
 
   // convenience getter for easy access to form fields
-  get f() { return this.form.controls; }
+  get f() {
+    return this.form.controls;
+  }
 
   editSection() {
     this.section = new ActSection(this.form.value);
     this.loading = true;
-    (this.service.edit(this.section)).pipe(
-      finalize(() => this.loading = false)
-    ).subscribe(() => this.editOk(), () => snackError(this.snackbar, 'Error al editar sección'));
+    this.service
+      .edit(this.section)
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe(
+        () => this.editOk(),
+        () => snackError(this.snackbar, "Error al editar sección")
+      );
   }
 
   editOk() {
-    snackOk(this.snackbar, 'Sección actualizada');
-    this.router.navigate(['home/sections'], { relativeTo: this.route });
+    snackOk(this.snackbar, "Sección actualizada");
+    this.router.navigate(["home/sections"], { relativeTo: this.route });
     this.dialogRef.close();
   }
-
 }
--- src/app/versions/list-versions/list-versions.component.ts
+++ src/app/versions/list-versions/list-versions.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { ActFormat } from '../../shared/models/actformat.model';
-import { PermitsService } from '../../core/services/permits.service';
-import { VersionsService } from '../services/versions.service';
-import { ActVersion } from 'src/app/shared/models/actversion.model';
-import { ActivatedRoute } from '@angular/router';
-import { AddVersionDialogComponent } from '../add-version-dialog/add-version-dialog.component';
-import { ConfirmDialogComponent } from 'src/app/shared/components/dialogconfirm/confirm-dialog.component';
-import { finalize } from 'rxjs/operators';
-import { snackOk, snackError } from 'src/app/util/snackbar-util';
-import { MatSnackBar } from '@angular/material';
-import { VersionSectionsDialogComponent } from '../version-sections-dialog/version-sections-dialog.component';
-import { VersionQuesDialogComponent } from '../version-ques-dialog/version-ques-dialog.component';
-import { FormatsService } from '../../formats/services/formats.service';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatDialog, MatDialogRef } from "@angular/material/dialog";
+import { ActFormat } from "../../shared/models/actformat.model";
+import { PermitsService } from "../../core/services/permits.service";
+import { VersionsService } from "../services/versions.service";
+import { ActVersion } from "src/app/shared/models/actversion.model";
+import { ActivatedRoute } from "@angular/router";
+import { AddVersionDialogComponent } from "../add-version-dialog/add-version-dialog.component";
+import { ConfirmDialogComponent } from "src/app/shared/components/dialogconfirm/confirm-dialog.component";
+import { finalize } from "rxjs/operators";
+import { snackOk, snackError } from "src/app/util/snackbar-util";
+import { MatSnackBar } from "@angular/material";
+import { VersionSectionsDialogComponent } from "../version-sections-dialog/version-sections-dialog.component";
+import { VersionQuesDialogComponent } from "../version-ques-dialog/version-ques-dialog.component";
+import { FormatsService } from "../../formats/services/formats.service";
 /*import { AddFormatDialogComponent } from '../add-format-dialog/add-format-dialog.component';
 import { EditFormatDialogComponent } from '../edit-format-dialog/edit-format-dialog.component';
 import { DelFormatDialogComponent } from '../del-format-dialog/del-format-dialog.component';
@@ -22,16 +22,21 @@ import { EditCompaniesDialogComponent } from '../edit-companies-dialog/edit-comp
 import { EditSectionsDialogComponent } from '../edit-sections-dialog/edit-sections-dialog.component';
 */
 @Component({
-  selector: 'app-list-versions',
-  templateUrl: './list-versions.component.html',
-  styleUrls: ['./list-versions.component.scss']
+  selector: "app-list-versions",
+  templateUrl: "./list-versions.component.html",
+  styleUrls: ["./list-versions.component.scss"],
 })
 export class ListVersionsComponent implements OnInit {
-
   data = new Array();
   dataForm = new Array();
   breadcrumb = new Array();
-  columnsFormat: string[] = ['codVersion', 'version', 'dateCreated', 'stateVersion', 'actions'];
+  columnsFormat: string[] = [
+    "codVersion",
+    "version",
+    "dateCreated",
+    "stateVersion",
+    "actions",
+  ];
   dataSource: MatTableDataSource<ActVersion>;
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
@@ -44,20 +49,31 @@ export class ListVersionsComponent implements OnInit {
    editSecDialogRef: MatDialogRef<EditSectionsDialogComponent>;*/
   index: number;
 
-  permAssignSections = this.servicePermits.validatePermit('Formatos.versiones.asignarSecciones');
-  permAssignQuestions = this.servicePermits.validatePermit('Formatos.versiones.asignarPreguntas');
-  permCreate = this.servicePermits.validatePermit('Formatos.versiones.crear');
-  permEdit = this.servicePermits.validatePermit('Formatos.versiones.editar');
-  permDel = this.servicePermits.validatePermit('Formatos.versiones.eliminar');
+  permAssignSections = this.servicePermits.validatePermit(
+    "Encuestas.versiones.asignarSecciones"
+  );
+  permAssignQuestions = this.servicePermits.validatePermit(
+    "Encuestas.versiones.asignarPreguntas"
+  );
+  permCreate = this.servicePermits.validatePermit("Encuestas.versiones.crear");
+  permEdit = this.servicePermits.validatePermit("Encuestas.versiones.editar");
+  permDel = this.servicePermits.validatePermit("Encuestas.versiones.eliminar");
 
   idFormat: number;
   nameFormat: number;
 
-  constructor(private service: VersionsService, public dialog: MatDialog, public servicePermits: PermitsService,
-    private activateRoute: ActivatedRoute, private snackbar: MatSnackBar, public serviceForm: FormatsService) { }
+  constructor(
+    private service: VersionsService,
+    public dialog: MatDialog,
+    public servicePermits: PermitsService,
+    private activateRoute: ActivatedRoute,
+    private snackbar: MatSnackBar,
+    public serviceForm: FormatsService
+  ) {}
 
-  ngOnInit() { // pedir explicación de luis
-    this.activateRoute.params.subscribe(params => {
+  ngOnInit() {
+    // pedir explicación de luis
+    this.activateRoute.params.subscribe((params) => {
       this.idFormat = params["id_format"];
       this.getFormat();
       this.list();
@@ -76,7 +92,7 @@ export class ListVersionsComponent implements OnInit {
   }
 
   list() {
-    this.service.list(this.idFormat).subscribe(list => {
+    this.service.list(this.idFormat).subscribe((list) => {
       this.data = list;
       this.dataSource = new MatTableDataSource(this.data);
       this.dataSource.paginator = this.paginator;
@@ -88,20 +104,20 @@ export class ListVersionsComponent implements OnInit {
   }
 
   getFormat() {
-    this.serviceForm.formById(this.idFormat).subscribe(format => {
+    this.serviceForm.formById(this.idFormat).subscribe((format) => {
       this.dataForm = format;
-      this.nameFormat = this.dataForm['nameFormat'];
+      this.nameFormat = this.dataForm["nameFormat"];
 
-        this.breadcrumb.push(
-          { url: '/home/homePage', name: 'Inicio' },
-          { url: '../../../../home/formats', name: 'Formatos' },
-          { url: '', name: `Versiones ${this.nameFormat}` },
-        );
+      this.breadcrumb.push(
+        { url: "/home/homePage", name: "Inicio" },
+        { url: "../../../../home/formats", name: "Formatos" },
+        { url: "", name: `Versiones ${this.nameFormat}` }
+      );
     });
   }
 
   startAdd(): void {
-    this.showDialog(null)
+    this.showDialog(null);
   }
 
   startEdit(i: number, element: any) {
@@ -112,23 +128,24 @@ export class ListVersionsComponent implements OnInit {
       dateCreated: new Date(element.date_created),
       codVersion: element.cod_version,
       stateVersion: element.state_ver,
-      version: element.version
-    }
-    this.showDialog(objEdit)
+      version: element.version,
+    };
+    this.showDialog(objEdit);
   }
 
   showDialog(objEdit: ActVersion) {
     this.addDialogRef = this.dialog.open(AddVersionDialogComponent, {
-      width: '400px',
-      height: 'auto',
+      width: "400px",
+      height: "auto",
       closeOnNavigation: true,
       autoFocus: false,
       data: {
-        idFormat: this.idFormat, objEdit: objEdit
-      }
+        idFormat: this.idFormat,
+        objEdit: objEdit,
+      },
     });
 
-    this.addDialogRef.afterClosed().subscribe(result => {
+    this.addDialogRef.afterClosed().subscribe((result) => {
       this.list();
     });
   }
@@ -136,66 +153,70 @@ export class ListVersionsComponent implements OnInit {
   startDelete(i: number, element: any) {
     this.index = i;
     this.confirmDialogRef = this.dialog.open(ConfirmDialogComponent, {
-      width: '400px',
-      height: 'auto',
+      width: "400px",
+      height: "auto",
       closeOnNavigation: true,
-      data: { message: `¿Esta seguro de eliminar la versión con código: ${element.cod_version}?` }
+      data: {
+        message: `¿Esta seguro de eliminar la versión con código: ${element.cod_version}?`,
+      },
     });
 
-    this.confirmDialogRef.afterClosed().subscribe(result => {
+    this.confirmDialogRef.afterClosed().subscribe((result) => {
       if (result) {
-        this.service.delete(element.id_version)
-          .subscribe(() => {
-            snackOk(this.snackbar, 'Versión eliminada')
-            this.list()
+        this.service.delete(element.id_version).subscribe(
+          () => {
+            snackOk(this.snackbar, "Versión eliminada");
+            this.list();
           },
-            () => snackError(this.snackbar, 'Error al eliminar versión'));
+          () => snackError(this.snackbar, "Error al eliminar versión")
+        );
         this.list();
       }
     });
   }
 
-
   startEditSec(i: number, element: any) {
     this.index = i;
-    this.versionSectionDialog = this.dialog.open(VersionSectionsDialogComponent, {
-      width: '80%',
-      height: 'auto',
-      closeOnNavigation: true,
-      autoFocus: false,
-      data: {
-        version: {
-          idVersion: element.id_version,
-          idFormat: element.id_format,
-          codVersion: element.cod_version,
-          version: element.version
-        }
+    this.versionSectionDialog = this.dialog.open(
+      VersionSectionsDialogComponent,
+      {
+        width: "80%",
+        height: "auto",
+        closeOnNavigation: true,
+        autoFocus: false,
+        data: {
+          version: {
+            idVersion: element.id_version,
+            idFormat: element.id_format,
+            codVersion: element.cod_version,
+            version: element.version,
+          },
+        },
       }
-    });
+    );
 
-    this.versionSectionDialog.afterClosed().subscribe(result => {
+    this.versionSectionDialog.afterClosed().subscribe((result) => {
       // this.list();
     });
   }
 
-
   startEditQues(i: number, element: any) {
     this.index = i;
     this.versionQuestionDialog = this.dialog.open(VersionQuesDialogComponent, {
-      width: '80%',
-      height: 'auto',
+      width: "80%",
+      height: "auto",
       closeOnNavigation: true,
       autoFocus: false,
       data: {
         version: {
           idVersion: element.id_version,
           codVersion: element.cod_version,
-          version: element.version
-        }
-      }
+          version: element.version,
+        },
+      },
     });
 
-    this.versionQuestionDialog.afterClosed().subscribe(result => {
+    this.versionQuestionDialog.afterClosed().subscribe((result) => {
       // this.list();
     });
   }
@@ -205,5 +226,4 @@ export class ListVersionsComponent implements OnInit {
     filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
     this.dataSource.filter = filterValue;
   }
-
 }
